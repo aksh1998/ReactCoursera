c000cc8dce636e20b51f2860b12b3c4e12cbc793
rathiakshay <akshay.rathi@intimetec.com>
2020-12-24 14:06:59 +0530

    Divide into component

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
 import {
-    Card, CardImg, CardImgOverlay, CardText, CardBody,
+    Card, CardImg, CardText, CardBody,
     CardTitle
 } from 'reactstrap';
 
 
 class DishDetail extends Component {
-
-    constructor(props) {
-        super(props);
-    }
     render() {
+        if (this.props.dish != null) {
+            const comments = this.props.dish.comments.map((comment) => {
+                return (
+                    <div>
+                        <div>
+                            <p>
+                                {comment.comment}
+                            </p>
+                        </div>
+                        <div>
+                            <p>
+                                --{comment.author} ,{comment.date}
+                            </p>
+                        </div>
+                    </div>
+                );
+            });
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <Card>
+                                <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+                                <CardBody>
+                                    <CardTitle>{this.props.dish.name}</CardTitle>
+                                    <CardText>{this.props.dish.description}</CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <h1>
+                                Comments
+                            </h1>
+                            <div>
+                                {comments}
+                            </div>
+                        </div>
+                    </div>
+                </div>);
+        }
         return (
-            <Card>
-                  <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
-                  <CardBody>
-                    <CardTitle>{this.props.dish.name}</CardTitle>
-                    <CardText>{this.props.dish.description}</CardText>
-                  </CardBody>
-            </Card>
-        );
+            <div></div>
+        )
     }
 }
 
