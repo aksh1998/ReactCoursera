a5d92b0713150ecf8d2b9a400d42b0cdd4cfc83a
rathiakshay <akshay.rathi@intimetec.com>
2020-12-24 16:23:35 +0530

    Functional component

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -1,56 +1,65 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
     Card, CardImg, CardText, CardBody,
     CardTitle
 } from 'reactstrap';
 
-
-class DishDetail extends Component {
-    render() {
-        if (this.props.dish != null) {
-            const comments = this.props.dish.comments.map((comment) => {
-                return (
-                    <div>
-                        <div>
-                            <p>
-                                {comment.comment}
-                            </p>
-                        </div>
-                        <div>
-                            <p>
-                                --{comment.author} ,{comment.date}
-                            </p>
-                        </div>
-                    </div>
-                );
-            });
-            return (
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            <Card>
-                                <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
-                                <CardBody>
-                                    <CardTitle>{this.props.dish.name}</CardTitle>
-                                    <CardText>{this.props.dish.description}</CardText>
-                                </CardBody>
-                            </Card>
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            <h1>
-                                Comments
-                            </h1>
-                            <div>
-                                {comments}
-                            </div>
-                        </div>
-                    </div>
-                </div>);
-        }
-        return (
-            <div></div>
-        )
-    }
+function RenderDish({ dish }) {
+    return (
+        <div className="col-12 col-md-5 m-1">
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
 }
+function RenderComments({ dishComments }) {
+    const comments = dishComments.map((comment) => {
+        return (
+            <div>
+                <div>
+                    <p>
+                        {comment.comment}
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        --{comment.author} ,{comment.date}
+                    </p>
+                </div>
+            </div>
+        );
+    });
+    return (
+        <div className="col-12 col-md-5 m-1">
+            <h1>
+                Comments
+        </h1>
+            <div>
+                {comments}
+            </div>
+        </div>
+    );
+}
+const DishDetail = (props) => {
+    if (props.dish != null) {
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <RenderDish dish={props.dish} />
+                    <RenderComments dishComments={props.dish.comments} />
+                </div>
+            </div>);
+    }
+    return (
+        <div></div>
+    )
+}
+
 
 export default DishDetail;
