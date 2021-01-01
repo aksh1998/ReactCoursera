3094be3566f4447f4e8f916eee99cfa794625775
rathiakshay <akshay.rathi@intimetec.com>
2021-01-01 10:10:28 +0530

    combineReducer

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import {
     Card, CardImg, CardText, CardBody,
     CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal,
