edc78cb6c51a418dad46313849b448ac49387888
rathiakshay <akshay.rathi@intimetec.com>
2021-01-02 10:31:59 +0530

    React-redux-form for form state

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,8 @@
-import {createStore, combineReducers,applyMiddleware} from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
@@ -13,7 +14,10 @@ export const ConfigureStore = () => {
             dishes: Dishes,
             comments: Comments,
             promotions: Promotions,
-            leaders: Leaders
+            leaders: Leaders,
+            ...createForms({
+                feedback: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
